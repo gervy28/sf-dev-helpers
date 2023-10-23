b00f59c232e41463200001310ea8dd3e93e95b48
Nate <nate@siteflight.co>
2023-10-23 11:38:41 -0500

    commented out, import-csv automatically runs

--- sf-dev-helpers.php
+++ sf-dev-helpers.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Plugin Name:       SiteFlight Mapped
+ * Plugin Name:       SiteFlight Dev Helpers
  * Description:       Get mapped provides a way to insert a custom map into your project. It connects to a CPT allowing you
  *                    to create a map of anything you like. It requires latitude and longitude.
  * Version:           1.0
@@ -22,4 +22,4 @@ define('SFDVHLP_DIR', plugin_dir_path(__FILE__));
 define('SFDVHLP_DIR_URL', plugin_dir_url(__FILE__));
 
 require SFDVHLP_DIR . 'console-log.php';
-require SFDVHLP_DIR . 'import-csv.php';
+// require SFDVHLP_DIR . 'import-csv.php';
